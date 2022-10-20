f49b66094082ac683e70d1d4c8fa0b30b03bdb7d
Ellaijemugbegabari <97686631+Ellaijemugbegabari@users.noreply.github.com>
2022-10-20 09:33:17 +0200

    knapp

--- Assets/PlayButton.cs
+++ Assets/PlayButton.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class PlayButton : MonoBehaviour
 {
   public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Spel");
     }
 }
